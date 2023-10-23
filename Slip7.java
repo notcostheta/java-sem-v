f580445691485d7ac7ba84fcc05435e93eeadf8d
notcostheta <kaustubhbhiwsankar@gmail.com>
2023-10-23 12:43:44 +0530

    fiexed 7

--- Slip7.java
+++ Slip7.java
@@ -2,66 +2,47 @@
 and Hockey_player from player class. The objective of this assignment is to learn the concepts of inheritance
 in Java. */
 
-class Player
-{
-	String gname;
-	Player(String gname)
-	{
-	    this.gname = gname;
-	}
-}
-class Cricket_player extends Player
-{
-	int no_of_player;
-	Cricket_player(String gname, int no_of_player)	
-	{
-	      super(gname);
-	      this.no_of_player = no_of_player;
-	}
-	void display()
-	{
-	     System.out.println("Game name = " + gname);
-	     System.out.println("Number of Player = " + no_of_player);
-	}
-}
-class Football_player extends Player
-{
-	int no_of_player;
-	Football_player(String gname, int no_of_player)	
-	{
-	      super(gname);
-	      this.no_of_player = no_of_player;
-	}
-	void display()
-	{
-	     System.out.println("Game name = " + gname);
-	     System.out.println("Number of Player = " + no_of_player);
-	}
-}
-class Hockey_player extends Player
-{
-	int no_of_player;
-	Hockey_player(String gname, int no_of_player)	
-	{
-	      super(gname);
-	      this.no_of_player = no_of_player;
-	}
-	void display()
-	{
-	     System.out.println("Game name = " + gname);
-	     System.out.println("Number of Player = " + no_of_player);
-	}
-}
+package slip7;
 
-class Slip7
-{
-    public static void main(String args[])	
-    {
-	Cricket_player c = new Cricket_player ("Cricket", 11);
-	Football_player d = new Football_player ("Football",11);
-	Hockey_player e = new Hockey_player ("Hockey",6);
-	c.display();
-	d.display();
-	e.display();			
+class Player {
+    String gname;
+
+    Player(String gname) {
+        this.gname = gname;
+    }
+
+    void display(int no_of_player) {
+        System.out.println("Game name = " + gname);
+        System.out.println("Number of Player = " + no_of_player);
     }
 }
+
+class Cricket_player extends Player {
+    Cricket_player(String gname, int no_of_player) {
+        super(gname);
+        display(no_of_player);
+    }
+}
+
+class Football_player extends Player {
+    Football_player(String gname, int no_of_player) {
+        super(gname);
+        display(no_of_player);
+    }
+}
+
+class Hockey_player extends Player {
+    Hockey_player(String gname, int no_of_player) {
+        super(gname);
+        display(no_of_player);
+    }
+}
+
+public class Slip7 {
+    public static void main(String[] args) {
+        Cricket_player c = new Cricket_player("Cricket", 11);
+        Football_player d = new Football_player("Football", 11);
+        Hockey_player e = new Hockey_player("Hockey", 6);
+    }
+}
+
